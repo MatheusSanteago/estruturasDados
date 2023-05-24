be578246a83bdb3a1acd72e7ef9de05b14b7ff17
MatheusSanteago <matheussanteago@gmail.com>
2023-05-24 11:50:01 -0300

    Adicionando Código

--- Ponteiros/alocacaodinamica.c
+++ Ponteiros/alocacaodinamica.c
@@ -1,3 +1,23 @@
-//
-// Created by mathe on 24/05/2023.
-//
+#include <stdio.h>
+#include <stdlib.h>
+
+int xi;
+int *pXi;
+
+int main(){
+    printf("Endereco: %p\n\n", pXi);
+    pXi = (int *) malloc(sizeof(int));
+    printf("Endereco: %p\nValor: %d\n\n", pXi,*pXi);
+    *pXi = 10;
+    printf("Endereco: %p\nValor: %d\n\n", pXi,*pXi);
+
+    int x;
+    x = 20;
+    pXi = &x;
+    printf("Endereco: %p\nValor: %d\n\n", pXi,*pXi);
+    pXi = (int *) malloc(sizeof(int));
+    printf("Endereco: %p\nValor: %d\n", pXi,*pXi);
+
+    system("Pause");
+    return (0);
+}
