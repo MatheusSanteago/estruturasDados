3fa9069b4489919bfe65888bdab4d64d0bcd20d3
MatheusSanteago <matheussanteago@gmail.com>
2023-05-26 12:08:04 -0300

    Adicionando exemplo de vetores dinamicos

--- Ponteiros/vetoresdinamicos.c
+++ Ponteiros/vetoresdinamicos.c
@@ -1,3 +1,19 @@
-//
-// Created by mathe on 24/05/2023.
-//
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int tam;
+int *vetor;
+
+int main(){
+    printf("Select the size of vector: ");
+    scanf("%d", &tam);
+    vetor = (int *) malloc(sizeof(int)*tam);
+
+    for(int i = 0; i < tam; i++){
+        vetor[i] = pow(2,i);
+        printf("Position: %d: %d\n",i,vetor[i]);
+    }
+    system("Pause");
+    return (0);
+}
